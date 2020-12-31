93ec5505113f99797c34a8d43ce7d69e1b6b8ee3
MS\kwakm <kwakms821@daum.net>
2020-12-31 23:35:56 +0900

    [ CI ] useMemo 사용해보기

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
-import React, { useRef, useState } from "react";
+import React, { useRef, useState,useMemo } from "react";
 import UserList from "./Components/UserList";
 import CreateUser from "./Components/CreateUser";
+
+function countActiveUsers(users){
+    console.log('사용자 수를 센다.')
+    return users.filter(user => user.active).length;
+}
 
 function App() {
     const [inputs, setInputs] = useState({
@@ -20,16 +25,19 @@ function App() {
             id: 1,
             username: "velopert",
             email: "public.velopert@gmail.com",
+            active: true,
         },
         {
             id: 2,
             username: "tester",
             email: "tester@example.com",
+            active: false,
         },
         {
             id: 3,
             username: "liz",
             email: "liz@example.com",
+            active: false,
         },
     ]);
 
@@ -54,10 +62,15 @@ function App() {
         // = user.id 가 id 인 것을 제거함
         setUsers(users.filter((user) => user.id !== id));
     };
+    const onToggle = (id) => {
+        setUsers(users.map((user) => (user.id === id ? { ...user, active: !user.active } : user)));
+    };
+  const count = useMemo(() => countActiveUsers(users),[users]);
     return (
         <>
             <CreateUser username={username} email={email} onChange={onChange} onCreate={onCreate} />
-            <UserList users={users} onRemove={onRemove} />
+            <UserList users={users} onRemove={onRemove} onToggle={onToggle}/>
+            <div>활성사용자 수: {count}</div>
         </>
     );
 }
